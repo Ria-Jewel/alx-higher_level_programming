5e32d8109c23b40d22ef1fd4b1ae7b4d9e1f6d15
Gloria John <glogloriajohn@gmail.com>
2023-08-08 13:55:21 -0700

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-# Author - Gloria John
-"""Write a function that prints the numbers from 1 to 100 separated by a space."""
-
-
 def fizzbuzz():
-    for number in range(1, 101):
-        if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz ", end="")
-        elif number % 3 == 0:
-            print("Fizz ", end="")
-        elif number % 5 == 0:
-            print("Buzz ", end="")
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 != 0:
+            print('Fizz', end=' ')
+        elif num % 5 == 0 and num % 3 != 0:
+            print('Buzz', end=' ')
+        elif num % 3 == 0 and num % 5 == 0:
+            print('FizzBuzz', end=' ')
         else:
-            print("{} ".format(number), end="")
+            print(num, end=' ')
