5a75db39db2debb0e54881f56473f171416b8752
Gloria John <glogloriajohn@gmail.com>
2023-08-08 13:05:59 -0700

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Gloria John
+
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - Gloria John
+
+def islower(c):
+    """Write a function that checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
