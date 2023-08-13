74318acc981f3cc146bdf809093f5860dea99b0b
Gloria John <glogloriajohn@gmail.com>
2023-08-13 09:26:01 -0700

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,78 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
-} listint_t;
+/**
+ * reverse_listint - Reverses a linked list.
+ * @head: A pointer to the first node in the list.
+ *
+ * Return: A pointer to the first node in the new list.
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-int is_palindrome(listint_t **head) {
-    if (*head == NULL) {
-        return 1; // An empty list is considered a palindrome
-    }
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
 
-    listint_t *slow = *head;
-    listint_t *fast = *head;
-    listint_t *prev = NULL;
-    listint_t *next = NULL;
-
-    // Move slow to the middle of the list using the two-pointer technique
-    while (fast != NULL && fast->next != NULL) {
-        fast = fast->next->next;
-        next = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = next;
-    }
-
-    // If the list has odd number of nodes, move slow one step forward
-    if (fast != NULL) {
-        slow = slow->next;
-    }
-
-    // Compare the first half (prev) and the second half (slow)
-    while (prev != NULL && slow != NULL) {
-        if (prev->n != slow->n) {
-            return 0; // Not a palindrome
-        }
-        prev = prev->next;
-        slow = slow->next;
-    }
-
-    return 1; // It's a palindrome
+	*head = prev;
 }
 
-// Helper function to create a new node
-listint_t *newNode(int n) {
-    listint_t *node = malloc(sizeof(listint_t));
-    if (node != NULL) {
-        node->n = n;
-        node->next = NULL;
-    }
-    return node;
-}
+/**
+ * is_palindrome - Checks if a linked list is a palindrome.
+ * @head: A double pointer to the linked list.
+ *
+ * Return: 1 if it is a palindrome, 0 if not.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-int main() {
-    listint_t *head = newNode(1);
-    head->next = newNode(2);
-    head->next->next = newNode(3);
-    head->next->next->next = newNode(2);
-    head->next->next->next->next = newNode(1);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-    int result = is_palindrome(&head);
-    if (result) {
-        printf("The list is a palindrome.\n");
-    } else {
-        printf("The list is not a palindrome.\n");
-    }
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
 
-    // Free the allocated memory
-    while (head != NULL) {
-        listint_t *temp = head;
-        head = head->next;
-        free(temp);
-    }
+	reverse_listint(&dup);
 
-    return 0;
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
 }
 
