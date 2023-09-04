f4035b85040ea1310298d4e450a4e0b6fd06ddff
Gloria John <glogloriajohn@gmail.com>
2023-09-04 19:29:41 +0000

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""an empty class Defines a Rectangle class."""
+
+
 class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
-
+    """Represent a rectanglar string."""
+    pass
